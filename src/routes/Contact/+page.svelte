5764c512b060867a5aa2e6870f2c57ec5cfdc804
sheep12341234 <seanyun775@gmail.com>
2025-10-15 01:14:49 +0000

    test

--- src/routes/Contact/+page.svelte
+++ src/routes/Contact/+page.svelte
@@ -1,47 +1,40 @@
-<div class = "green-box">
-  <div class = "heading">Request</div>
-  <hr>
-  <div class = "subheading">Have any ideas</div>
-  <div class = "subheading">for genres?</div>
-<br>
-<br>
-  <div class = textbox>
-  <div class = "text">Send me an email at: seanyun@gmail.com</div>
-  <br>
-  <a href = "/Home">Back</a>
-  </div>
+<div class="green-box">
+	<div class="heading">Requests</div>
+	<hr />
+	<div class="subheading">Have any ideas</div>
+	<div class="subheading">for genres?</div>
+	<br />
+	<br />
+	<div class="textbox">
+		<div class="text">Send me an email at: seanyun@gmail.com</div>
+		<br />
+		<a href="/Home">Back</a>
+	</div>
 </div>
 
 <style>
-    .green-box{
-  height: 500px;
-  width: 300px;
-  background-color: lawngreen;
-  border-radius: 30px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 50px auto;
-}
+	.green-box {
+		height: 500px;
+		width: 300px;
+		background-color: lawngreen;
+		border-radius: 30px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		margin: 50px auto;
+	}
 
-.heading{
-  padding: 5px;
-  font-size: 50px;
-  font-weight: bold;
-}
-.subheading{
-  font-size: 40px;
-}
+	.heading {
+		padding: 5px;
+		font-size: 50px;
+		font-weight: bold;
+	}
+	.subheading {
+		font-size: 40px;
+	}
 
-.text{
-  font-size: 24px;
-}
-
-
-.textbox{
-  width: 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+	.text {
+		font-size: 24px;
+	}
 </style>
